0d438299eee8f1566f264b6ed291c285051f2969
Ketan <ketanap@gmail.com>
2023-08-09 09:07:46 +0530

    Token

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -5,6 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebApiDemo.Models;
 using WebApiDemo.ViewModels;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace WebApiDemo.Controllers
 {
@@ -30,16 +34,38 @@ namespace WebApiDemo.Controllers
 
         [Route("Login")]
         [HttpPost]
-        public string Login(VMLogin u)
+        public IActionResult Login(VMLogin u)
         {
+            IConfigurationRoot _configuration = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json")
+               .Build();
             var udata= context.tblUser.Where (eu=>eu.Email ==u.Email && eu.Password ==u.Password ).FirstOrDefault();
             if(udata!=null)
             {
-                return "Success";
+                //return "Success";
+                  var claims = new[] {
+                        new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                        new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                        new Claim("UserId", udata.UserId.ToString()),
+                        new Claim("UserName", udata.UserName),
+                    };
+
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+                    var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                    var token = new JwtSecurityToken(
+                        _configuration["Jwt:Issuer"],
+                        _configuration["Jwt:Audience"],
+                        claims,
+                        expires: DateTime.UtcNow.AddMinutes(10),
+                        signingCredentials: signIn);
+
+                    return Ok(new JwtSecurityTokenHandler().WriteToken(token));
             }
             else
             {
-                return "Invalid";
+                return BadRequest("Invalid credentials");
             }
         }
 
