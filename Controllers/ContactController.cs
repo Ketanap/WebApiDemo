7f9f70c72f87df1e3c6697b8a082b98aa6371e11
Ketan <ketanap@gmail.com>
2023-08-10 09:17:20 +0530

    Complate

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebApiDemo.Models;
 using WebApiDemo.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace WebApiDemo.Controllers
 {
@@ -14,22 +16,28 @@ namespace WebApiDemo.Controllers
     {
         ApiDBContext context = new ApiDBContext();
         [HttpGet]
+        [Authorize]
         public IEnumerable<tblContact> Get()
         {
-               return context.tblContact.ToList(); 
+            var UserId = HttpContext.User.Claims.First(x => x.Type == "UserId").Value;
+            return context.tblContact.Where( con =>con.UserId==Convert.ToInt32( UserId )).ToList(); 
         }
 
         [HttpGet("{id}")]
+        [Authorize]
         public tblContact Get(int id)
         {
+             var UserId = HttpContext.User.Claims.First(x => x.Type == "UserId").Value;
                return context.tblContact.Where(
-                    data =>data.ContactId==id).FirstOrDefault();; 
+                    data =>data.ContactId==id && data.UserId==Convert.ToInt32( UserId)).FirstOrDefault(); 
         }
         [HttpPost]
+        [Authorize]
         public string Insert(VMContact data)
         {
+             var UserId = HttpContext.User.Claims.First(x => x.Type == "UserId").Value;
             tblContact newContact=new tblContact();
-            newContact.UserId=data.UserId;
+            newContact.UserId=Convert.ToInt32( UserId);
             newContact.Name=data.Name;
             newContact.Address =data.Address ;
             newContact.Email = data.Email ;
@@ -40,6 +48,7 @@ namespace WebApiDemo.Controllers
         }
 
         [HttpPut("{id}")]
+        [Authorize]
         public string Update(VMContact data,int id)
         {
            var editContact=context.tblContact.Where(
@@ -53,6 +62,7 @@ namespace WebApiDemo.Controllers
         }
 
         [HttpDelete("{id}")]
+        [Authorize]
         public string Delete(int id)
         {
              var delContact=context.tblContact.Where(
--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Microsoft.OpenApi.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,7 +28,33 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 builder.Services.AddDbContext<ApiDBContext>(options =>
             options.UseSqlServer("Data Source=PCC-SERVER\\SQLEXPRESS;Initial Catalog=ApiDB;Integrated Security=true;Encrypt=False"));
 
